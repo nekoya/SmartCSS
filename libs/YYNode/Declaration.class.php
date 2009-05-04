583f17fbd9e80d8ac9bf693056f054e5843d29e6
Ryo Miyake <ryo.studiom@gmail.com>
2009-05-04 23:48:50 +0900

    Print property and expr in declaration.

--- libs/YYNode/Declaration.class.php
+++ libs/YYNode/Declaration.class.php
@@ -19,8 +19,10 @@ class YYNode_Declaration extends YYNode {
      *
      */
     public function dump($indent) {
-        $property = $this->items[0]->value;
-        echo str_repeat(' ', $indent) . 'declaration:' . $this->id . ':' . $property . "\n";
+        list($property, $expr) = $this->items;
+        echo str_repeat(' ', $indent) . 'declaration:' . $this->id . "\n";
+        echo str_repeat(' ', $indent + Parser::indent) . 'property:' . $property->id . ':' . $property->value . "\n";
+        echo str_repeat(' ', $indent + Parser::indent) . 'expr:' . $expr->id . ':' . $expr->value . "\n";
         if ($this->hasNext()) {
             $this->next->dump($indent);
         }
