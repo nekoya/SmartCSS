c73dbc66af4b9318d8f57ccd3335a07aac41f867
Ryo Miyake <ryo.studiom@gmail.com>
2009-05-05 16:35:12 +0900

    Oops

--- scss.php
+++ scss.php
@@ -85,9 +85,9 @@ define('YYTERMS', 17);
 define('YYNONTERMS', 18);
 
 $yyaction = array(
-     48,   47,   49,    0,   50,   39,   26,   27,   23,   16,
-     28,    2,   14,   35,   16,   34,   40,    4,   45,-32766,
-      0,    0,   52,   12,  -27
+     47,   46,   48,    0,   49,   38,   25,   26,   22,   15,
+     27,    2,   13,   34,   15,   33,   39,    4,   44,-32766,
+      0,    0,    0,    7,  -27
   );
 
 define('YYLAST', 25);
@@ -95,27 +95,26 @@ define('YYLAST', 25);
 $yycheck = array(
       4,    5,    6,    0,    8,    7,   12,   13,    3,    4,
      16,    2,   14,   15,    4,    4,    4,    9,   11,   15,
-     -1,   -1,   16,   10,   10
+     -1,   -1,   -1,   10,   10
   );
 
 $yybase = array(
       0,    5,   10,   11,   11,   11,    8,   -4,   -6,    4,
-      4,    4,    6,    3,   12,    9,   14,   13,    7,    0,
-     -2,   -2,   -2,   -2,   -2,   -6,    0,    0,   -2,   -2,
-     -2
+      4,    4,    3,   12,    9,   14,   13,    7,    0,   -2,
+     -2,   -2,   -2,   -2,   -6,    0,    0,   -2,   -2,   -2
   );
 
 define('YY2TBLSTATE', 12);
 
 $yydefault = array(
       2,32767,32767,    1,32767,32767,    5,32767,    6,   12,
-     13,   14,   32,32767,32767,32767,   15,32767,32767
+     13,   14,32767,32767,32767,   15,32767,32767
   );
 
 
 
 $yygoto = array(
-     37,   37,   22,   41,   11,    0,    8,    0,   30
+     36,   36,   21,   40,   11,    0,    8,    0,   29
   );
 
 define('YYGLAST', 9);
@@ -130,26 +129,26 @@ $yygbase = array(
   );
 
 $yygdefault = array(
-  -32768,   13,    3,   44,   15,    1,    6,    5,   29,    9,
-     10,   36,   38,   42,   43,   17,    7,   18
+  -32768,   12,    3,   43,   14,    1,    6,    5,   28,    9,
+     10,   35,   37,   41,   42,   16,   17,-32768
   );
 
 $yylhs = array(
       0,    1,    2,    2,    3,    4,    4,    7,    7,    7,
       6,    6,    8,    8,    8,    9,    9,   10,   10,   11,
-     11,   12,    5,    5,   13,   13,   14,   15,   17,   17,
-     17,   17,   16,   16
+     11,   12,    5,    5,   13,   13,   14,   15,   16,   16,
+     16,   16,   17,   17
   );
 
 $yylen = array(
       1,    1,    0,    2,    4,    1,    3,    1,    1,    1,
       1,    3,    1,    1,    2,    1,    1,    1,    2,    1,
-      1,    2,    1,    2,    1,    1,    5,    1,    1,    1,
+      1,    2,    1,    2,    1,    1,    4,    1,    1,    1,
       1,    1,    0,    1
   );
 
-define('YYSTATES', 43);
-define('YYNLSTATES', 19);
+define('YYSTATES', 41);
+define('YYNLSTATES', 18);
 define('YYINTERRTOK', 1);
 define('YYUNEXPECTED', 32767);
 define('YYDEFAULT', -32766);
@@ -256,7 +255,7 @@ function yyparse()
         case 23:
 { $yyval = Parser::getInstance()->catNode($yyastk[$yysp-(2-1)], $yyastk[$yysp-(2-2)]); } break;
         case 26:
-{ $yyval = Parser::getInstance()->genDeclaration($yyastk[$yysp-(5-1)], $yyastk[$yysp-(5-3)]); } break;
+{ $yyval = Parser::getInstance()->genDeclaration($yyastk[$yysp-(4-1)], $yyastk[$yysp-(4-3)]); } break;
         case 27:
 { $yyval = Parser::getInstance()->genProperty($yyastk[$yysp-(1-1)]); } break;
         case 28:
