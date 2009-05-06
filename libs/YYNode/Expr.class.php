4ac34b09ecf9f43535439a16de03dd0ce112eb59
Ryo Miyake <ryo.studiom@gmail.com>
2009-05-06 21:03:32 +0900

    Fixed problem, does not publish multi expressions.

--- libs/YYNode/Expr.class.php
+++ libs/YYNode/Expr.class.php
@@ -3,4 +3,14 @@
  *
  */
 class YYNode_Expr extends YYNode {
+    /**
+     *
+     */
+    public function publish() {
+        $output = $this->value;
+        if ($this->hasNext()) {
+            $output .= ' ' . $this->next->publish();
+        }
+        return $output;
+    }
 }
