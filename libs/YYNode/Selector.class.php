d89c0be9cfa91c7e23bc07aeed1651cd70ba4cf4
Ryo Miyake <ryo.studiom@gmail.com>
2009-05-06 20:00:08 +0900

    Deleted publish method unused.

--- libs/YYNode/Selector.class.php
+++ libs/YYNode/Selector.class.php
@@ -6,22 +6,6 @@ class YYNode_Selector extends YYNode {
     /**
      *
      */
-    public function publish() {
-        $output = $this->value;
-        if ($this->hasNext()) {
-            $output .= ',' . $this->next->value;
-        }
-        $output .= ' { ';
-        if ($this->hasChildren()) {
-            $output .= $this->children->publish();
-        }
-        $output .= " }\n";
-        return $output;
-    }
-
-    /**
-     *
-     */
     public function dump($indent) {
         return str_repeat(' ', $indent * 2) . 'selector:' . $this->id . ':' . $this->value . "\n";
     }
