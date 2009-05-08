c5592de7b6e6dabc372dd34eea0f716239e6f0bb
Ryo Miyake <ryo.studiom@gmail.com>
2009-05-09 02:37:20 +0900

    Initialize test.

--- t/parser.php
+++ t/parser.php
@@ -1,7 +1,3 @@
 <?php
 require 'utils.php';
-$t->ok( Parser::getInstance()->addExpr('10px') );
-$t->ok( $parser = Parser::getInstance() );
-$node = $parser->nodes[0];
-$t->isa_ok( $node, 'YYNode_Expr' );
-$t->is( $node->publish(), '10px' );
+$t->ok( $parser = SCSS_Parser::getInstance() );
