c821654de725a13f3ba2dab84b371b5a54151162
Ryo Miyake <ryo.studiom@gmail.com>
2009-05-10 01:43:34 +0900

    Fixed mistakes of selector tests.

--- t/selector.php
+++ t/selector.php
@@ -6,8 +6,8 @@ $t->ok( $parser = SCSS_Parser::getInstance(), 'get parser instance' );
 $t->isa_ok( $parser, SCSS_Parser, 'parser instance isa SCSS_Parser' );
 
 $t->comment( 'create selector node' );
-$t->ok( $node = $parser->genSelector('div#header'), 'generate node' );
-$t->is( $decl->getType(), 'selector', 'get node type' );
-$t->ok( $decl->hasChildren() === false, 'has not child nodes' );
-$t->ok( $decl->hasNext() === false, 'has not next node' );
-$t->is( $decl->publish(), 'div#header', 'publish' );
+$t->ok( $sel = $parser->genSelector('div#header'), 'generate node' );
+$t->is( $sel->getType(), 'selector', 'get node type' );
+$t->ok( $sel->hasChildren() === false, 'has not child nodes' );
+$t->ok( $sel->hasNext() === false, 'has not next node' );
+$t->is( $sel->publish(), 'div#header', 'publish' );
