08b88d691c77933a2bd4a1936599fbd076a9a656
Ryo Miyake <ryo.studiom@gmail.com>
2009-05-29 01:30:44 +0900

    Supported recursive rulesets again

--- t/01-lexer.php
+++ t/01-lexer.php
@@ -7,30 +7,26 @@ $t->isa_ok( $lexer, 'SCSS_Lexer', 'parser instance isa SCSS_Lexer' );
 /*
  * lexical analyze
  */
-function isToken($buffer, $token, $diag = null) {
+function isToken($buffer, $tokenstr, $diag = null) {
     global $t, $lexer;
     $lexer->setBuffer($buffer);
     $results = array();
+    $tokens = array();
+    foreach (explode(' ', $tokenstr) as $token) {
+        array_push($tokens, $token);
+    }
     while ($result = $lexer->yylex()) {
         array_push( $results,
             is_numeric($result) ? chr($result) : $result
         );
     }
     if (is_null($diag)) {
-        if (is_array($token)) {
-            $diag = 'tokens: ' . join(' ', $token);
-        } else {
-            $diag = "token: $token";
-        }
+        $diag = "token: $tokenstr";
     }
-    if (count($results) < 2) {
-        $t->is( $results[0], $token, $diag );
-    } else {
-        $t->is( $results, $token, $diag );
-    }
+    $t->is( $results, $tokens, $diag );
 }
 
-$t->comment( 'test lexical analyze' );
+$t->comment( 'skip comments' );
 $lexer->setBuffer('/* comment */');
 $t->is( $lexer->yylex(), null, 'skip comment' );
 $lexer->setBuffer('/*** comment ***/');
@@ -38,58 +34,62 @@ $t->is( $lexer->yylex(), null, 'skip comment end of **/' );
 $lexer->setBuffer("/**\n * comment\n **/\n");
 $t->is( $lexer->yylex(), null, 'skip multi lines comment' );
 
+$t->comment( 'charset' );
 isToken('@charset "utf-8";', 'CHARSET');
 
+$t->comment( 'import' );
 $lexbuf = <<<__CSS__
 /* import */
 @import "base.css";
 @import url('http://www.example.com/print.css') print;
 __CSS__;
-isToken($lexbuf, array('IMPORT', 'IMPORT'));
+isToken($lexbuf, 'IMPORT IMPORT');
 
-isToken(
-    '* { margin:0; }',
-    array('SELECTOR', 'LBRACE', 'SPACE', 'IDENT', ':', 'NUMBER', ';', 'SPACE', 'RBRACE')
+$t->comment( 'rulesets' );
+isToken( '* { margin:0; }',
+    'SELECTOR LBRACE SPACE IDENT : NUMBER ; SPACE RBRACE'
 );
 
-isToken(
-    'div{margin:5px 10px}',
-    array('SELECTOR', 'LBRACE', 'IDENT', ':', 'LENGTH', 'SPACE', 'LENGTH', 'RBRACE')
+isToken( 'div{margin:5px 10px}',
+    'SELECTOR LBRACE IDENT : LENGTH SPACE LENGTH RBRACE'
 );
 
-isToken(
-    "p {\nline-height:1.5;\n}\n",
-    array('SELECTOR', 'LBRACE', 'IDENT', ':', 'NUMBER', ';', 'RBRACE')
+isToken( "a:hover {\nline-height:1.5;\n}\n",
+    'SELECTOR LBRACE IDENT : NUMBER ; RBRACE'
 );
 
-isToken(
-    'ul,ol { margin:0; padding:0; }',
-    array(
-        'SELECTOR', 'COMMA', 'SELECTOR',
-        'LBRACE', 'SPACE',
-        'IDENT', ':', 'NUMBER', ';', 'SPACE',
-        'IDENT', ':', 'NUMBER', ';', 'SPACE',
-        'RBRACE',
-    )
+isToken( 'ul,ol { margin:0; padding:0; }',
+    'SELECTOR COMMA SELECTOR '.
+    'LBRACE SPACE '.
+    'IDENT : NUMBER ; SPACE '.
+    'IDENT : NUMBER ; SPACE '.
+    'RBRACE'
 );
 
-isToken(
-    'h2+p  ,  div > span { margin: 0; padding : 0 ; }',
-    array(
-        'SELECTOR', 'COMMA', 'SPACE', 'SELECTOR',
-        'LBRACE', 'SPACE',
-        'IDENT', ':', 'SPACE', 'NUMBER', ';', 'SPACE',
-        'IDENT', 'SPACE', ':', 'SPACE', 'NUMBER', 'SPACE', ';', 'SPACE',
-        'RBRACE',
-    )
+isToken( 'h2+p  ,  div > span { margin: 0; padding : 0 ; }',
+    'SELECTOR COMMA SPACE SELECTOR '.
+    'LBRACE SPACE '.
+    'IDENT : SPACE NUMBER ; SPACE '.
+    'IDENT SPACE : SPACE NUMBER SPACE ; SPACE '.
+    'RBRACE'
 );
-exit;
 
-isToken(
-    'div { width:100%; p { color:#3399ff; } }',
-    array(
-        'SELECTOR', 'LBRACE', 'DECLARATION',
-        'SELECTOR', 'LBRACE', 'DECLARATION', 'RBRACE',
-        'RBRACE',
-    )
+$t->comment( 'recursive rulesets' );
+isToken( 'div { width:100%; p { color:#3399ff; } }',
+    'SELECTOR LBRACE SPACE IDENT : PERCENTAGE ; SPACE '.
+    'SELECTOR LBRACE SPACE IDENT : HEXCOLOR ; SPACE RBRACE SPACE '.
+    'RBRACE'
 );
+
+isToken( 'div{a:hover{color:#3399ff}}',
+    'SELECTOR LBRACE '.
+    'SELECTOR LBRACE IDENT : HEXCOLOR RBRACE '.
+    'RBRACE'
+);
+
+isToken( 'div { ul { list-style:none } margin:0 }',
+    'SELECTOR LBRACE SPACE '.
+    'SELECTOR LBRACE SPACE IDENT : IDENT SPACE RBRACE '.
+    'SPACE IDENT : NUMBER SPACE RBRACE'
+);
+
