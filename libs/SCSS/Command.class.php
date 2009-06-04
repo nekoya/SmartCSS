b3dfd50ce61b5f45e1c0d6ec02703a6d9442060b
Ryo Miyake <ryo.studiom@gmail.com>
2009-06-04 16:09:44 +0900

    Added Command base class

---
+++ libs/SCSS/Command.class.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ *
+ */
+abstract class SCSS_Command {
+    /**
+     *
+     */
+    public function __construct() {
+    }
+}
---
+++ t/11-command.php
@@ -0,0 +1,13 @@
+<?php
+require 'utils.php';
+$t->plan = 3;
+$t->comment( 'test parser instance' );
+$t->ok( $parser = SCSS_Parser::getInstance(), 'get parser instance' );
+$t->isa_ok( $parser, 'SCSS_Parser', 'parser instance isa SCSS_Parser' );
+
+$t->comment( 'not implemented command' );
+try {
+    $parser->genCommand('NONE');
+} catch (Exception $e) {
+    $t->is( $e->getMessage(), 'Command not found: NONE', 'Command not found: NONE' );
+}
