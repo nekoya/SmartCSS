5b1ab46200789580021c201e52a4298ab5822fb7
Ryo Miyake <ryo.studiom@gmail.com>
2009-06-04 16:08:45 +0900

    Modified specs

--- t/10-variable.php
+++ t/10-variable.php
@@ -6,5 +6,5 @@ $t->isa_ok( $parser, 'SCSS_Parser', 'parser instance isa SCSS_Parser' );
 
 $t->comment( 'test variables' );
 $t->is( $parser->getVar('hoge'), '', 'get undefined variable is ""' );
-$t->is( $parser->setVar('hoge', 'fuga'), 'fuga', 'set variable, return value' );
+$t->is( $parser->setVar('hoge', '"fuga"'), 'fuga', 'set variable, return value' );
 $t->is( $parser->getVar('hoge'), 'fuga', 'get variable' );
