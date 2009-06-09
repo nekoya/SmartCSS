508b8b3b4cbb15359307cacd9fc1562b7e1b6d71
Ryo Miyake <ryo.studiom@gmail.com>
2009-06-10 07:57:40 +0900

    Moved sample file

--- t/14-getopt_web.php
+++ t/14-getopt_web.php
@@ -9,10 +9,10 @@ $t->ok( $c = new SCSS_Getopt_Web(), 'create SCSS_Getopt_Web instance' );
 $t->isa_ok( $c, 'SCSS_Getopt_Web', 'instance isa SCSS_Getopt_Web' );
 
 $t->comment( 'getTargetFile' );
-$t->ok( $c->getTargetFile('../hoge.css'), 'get scss, parameter extension is .css' );
+$t->ok( $c->getTargetFile('../sample/hoge.css'), 'get scss, parameter extension is .css' );
 
 $t->comment( 'fail getTargetFile' );
 $t->is( $c->getTargetFile(''), false, 'empty args' );
-$t->is( $c->getTargetFile('none.css'), false, 'css file not found' );
-$t->is( $c->getTargetFile('hoge.scss'), false, 'target file extension must be .css' );
+$t->is( $c->getTargetFile('../sample/none.css'), false, 'css file not found' );
+$t->is( $c->getTargetFile('../sample/hoge.scss'), false, 'target file extension must be .css' );
 $t->is( $c->getTargetFile('../../../../../../etc/passwd'), false, 'cannot get upper basedir (acutually extension error)' );
