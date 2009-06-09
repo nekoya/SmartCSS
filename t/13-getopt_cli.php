7eadbb4c08920aa6afcd4e7b6ba54211dc8c4c15
Ryo Miyake <ryo.studiom@gmail.com>
2009-06-10 07:56:56 +0900

    Moved sample files to individual dir

--- t/13-getopt_cli.php
+++ t/13-getopt_cli.php
@@ -9,10 +9,10 @@ $t->ok( $c = new SCSS_Getopt_CLI(), 'create SCSS_Getopt_CLI instance' );
 $t->isa_ok( $c, 'SCSS_Getopt_CLI', 'instance isa SCSS_Getopt_CLI' );
 
 $t->comment( 'getTargetFile' );
-$t->ok( $c->getTargetFile('../hoge.scss'), 'get scss' );
+$t->ok( $c->getTargetFile('../sample/hoge.scss'), 'get scss' );
 
 $t->comment( 'fail getTargetFile' );
 $t->is( $c->getTargetFile(''), false, 'empty args' );
-$t->is( $c->getTargetFile('none.scss'), false, 'scss file not found' );
-$t->is( $c->getTargetFile('hoge.css'), false, 'target file extension must be .scss' );
+$t->is( $c->getTargetFile('../sample/none.scss'), false, 'scss file not found' );
+$t->is( $c->getTargetFile('../sample/hoge.css'), false, 'target file extension must be .scss' );
 $t->is( $c->getTargetFile('../../../../../../etc/passwd'), false, 'cannot get upper basedir (acutually extension error)' );
