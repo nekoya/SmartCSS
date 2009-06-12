216283c18cf0affd33df8fe6091812eb6200c7f9
Ryo Miyake <ryo.studiom@gmail.com>
2009-06-12 13:29:38 +0900

    updated tests (rename getTargetFile -> getRealPath)

--- t/13-getopt_cli.php
+++ t/13-getopt_cli.php
@@ -8,11 +8,11 @@ $t->output = new lime_output_color();
 $t->ok( $c = new SCSS_Getopt_CLI(), 'create SCSS_Getopt_CLI instance' );
 $t->isa_ok( $c, 'SCSS_Getopt_CLI', 'instance isa SCSS_Getopt_CLI' );
 
-$t->comment( 'getTargetFile' );
-$t->ok( $c->getTargetFile('../sample/sample.scss'), 'get scss' );
+$t->comment( 'getRealPath' );
+$t->ok( $c->getRealPath('../sample/sample.scss'), 'get scss' );
 
-$t->comment( 'fail getTargetFile' );
-$t->is( $c->getTargetFile(''), false, 'empty args' );
-$t->is( $c->getTargetFile('../sample/none.scss'), false, 'scss file not found' );
-$t->is( $c->getTargetFile('../sample/sample.css'), false, 'target file extension must be .scss' );
-$t->is( $c->getTargetFile('../../../../../../etc/passwd'), false, 'cannot get upper basedir (acutually extension error)' );
+$t->comment( 'fail getRealPath' );
+$t->is( $c->getRealPath(''), false, 'empty args' );
+$t->is( $c->getRealPath('../sample/none.scss'), false, 'scss file not found' );
+$t->is( $c->getRealPath('../sample/sample.css'), false, 'target file extension must be .scss' );
+$t->is( $c->getRealPath('../../../../../../etc/passwd'), false, 'cannot get upper basedir (acutually extension error)' );
--- t/14-getopt_web.php
+++ t/14-getopt_web.php
@@ -8,11 +8,11 @@ $t->output = new lime_output_color();
 $t->ok( $c = new SCSS_Getopt_Web(), 'create SCSS_Getopt_Web instance' );
 $t->isa_ok( $c, 'SCSS_Getopt_Web', 'instance isa SCSS_Getopt_Web' );
 
-$t->comment( 'getTargetFile' );
-$t->ok( $c->getTargetFile('../sample/sample.css'), 'get scss, parameter extension is .css' );
+$t->comment( 'getRealPath' );
+$t->ok( $c->getRealPath('../sample/sample.css'), 'get scss, parameter extension is .css' );
 
-$t->comment( 'fail getTargetFile' );
-$t->is( $c->getTargetFile(''), false, 'empty args' );
-$t->is( $c->getTargetFile('../sample/none.css'), false, 'css file not found' );
-$t->is( $c->getTargetFile('../sample/sample.scss'), false, 'target file extension must be .css' );
-$t->is( $c->getTargetFile('../../../../../../etc/passwd'), false, 'cannot get upper basedir (acutually extension error)' );
+$t->comment( 'fail getRealPath' );
+$t->is( $c->getRealPath(''), false, 'empty args' );
+$t->is( $c->getRealPath('../sample/none.css'), false, 'css file not found' );
+$t->is( $c->getRealPath('../sample/sample.scss'), false, 'target file extension must be .css' );
+$t->is( $c->getRealPath('../../../../../../etc/passwd'), false, 'cannot get upper basedir (acutually extension error)' );
