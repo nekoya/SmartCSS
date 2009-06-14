62d111d46e0f8fddf95e8b2f122bdbc7955bc35d
Ryo Miyake <ryo.studiom@gmail.com>
2009-06-14 20:33:53 +0900

    updated utils.php

--- t/utils.php
+++ t/utils.php
@@ -1,6 +1,6 @@
 <?php
-require 'lime.php';
-require '../libs/SCSS/AutoLoader.class.php';
-$t = new lime_test();
+require 'lime/pluggable.php';
+require dirname(__FILE__) . '/../libs/SCSS/AutoLoader.class.php';
+$t = new lime_test_pluggable();
 $t->output = new lime_output_color();
-$t->ok( $parser = new SCSS_Parser(), 'create parser instance' );
+$t->loadPlugins(array('exception', 'strict'));
