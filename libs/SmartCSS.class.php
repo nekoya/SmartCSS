1546c56d827f96cc9c898feca0e3745d7fd8a618
Ryo Miyake <ryo.studiom@gmail.com>
2009-06-14 23:19:34 +0900

    Parser and Lexer move to property.

--- libs/SmartCSS.class.php
+++ libs/SmartCSS.class.php
@@ -6,15 +6,20 @@ class SmartCSS {
     protected $content;
 
     public function run() {
+        $this->parser = new SCSS_Parser();
+        $this->lexer  = new SCSS_Lexer();
+
         if ($this->callAsCLI()) {
             $c = new SCSS_Getopt_CLI();
         } else {
             $c = new SCSS_Getopt_Web();
         }
+
         $this->fileName = $c->getParams();
         $realpath = $c->getRealPath($this->fileName);
         if ($realpath === false ) $c->failedReadFile();
         chdir(dirname($realpath));
+
         $buffer = file_get_contents($realpath);
         $this->content = $this->parseSCSS($buffer);
         $c->publish($this->content);
@@ -31,16 +36,14 @@ class SmartCSS {
     }
 
     public function parseSCSS($buffer) {
-        $parser = new SCSS_Parser();
-        $lexer  = new SCSS_Lexer();
-        $lexer->setBuffer($buffer);
+        $this->lexer->setBuffer($buffer);
         if ($this->debug) {
-            //$lexer->debug = true;
-            $parser->debug = true;
+            $this->lexer->debug = true;
+            $this->parser->debug = true;
         }
         try {
-            $parser->yyparse($lexer);
-            return $parser->run();
+            $this->parser->yyparse($this->lexer);
+            return $this->parser->run();
         } catch ( Exception $e ) {
             die('[ERROR]' . $e->getMessage() . PHP_EOL);
         }
